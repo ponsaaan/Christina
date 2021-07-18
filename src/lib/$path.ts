803b47c364362a1c7b2399761cfc1a27008d4ce1
ponsaaan <mttkd399@gmail.com>
2021-07-18 22:08:26 +0900

    ルーティングライブラリ設定

---
+++ src/lib/$path.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+// prettier-ignore
+export const pagesPath = {
+  posts: {
+    first_post: {
+      $url: (url?: { hash?: string }) => ({ pathname: '/posts/first-post' as const, hash: url?.hash })
+    }
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
+
+// prettier-ignore
+export type PagesPath = typeof pagesPath
+
+// prettier-ignore
+export const staticPath = {
+  favicon_ico: '/favicon.ico',
+  vercel_svg: '/vercel.svg'
+} as const
+
+// prettier-ignore
+export type StaticPath = typeof staticPath
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,4 +3,5 @@ import type { AppProps } from 'next/app';
 const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
+
 export default MyApp;
