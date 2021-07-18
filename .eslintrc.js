eb3f3708443047b93d320f82110e6b7821d0d2dc
ponsaaan <mttkd399@gmail.com>
2021-07-18 17:41:25 +0900

    settings

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+const prettierrc = require('./.prettierrc.js');
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'prettier',
+    '@typescript-eslint',
+    'jsx-a11y',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      prettierrc,
+      {
+        usePrettierrc: false,
+      },
+    ],
+    'no-console': 'warn',
+    'no-undef': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/prop-types': 'off',
+    'react/no-children-prop': 'off',
+    'jsx-a11y/no-onchange': 'off',
+  },
+};
---
+++ src/consts/color.tsx
@@ -0,0 +1,1 @@
+export const sampleBlack = '#313131';
