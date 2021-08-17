5295bf50452b94a4518f5395631930f471ef57e5
YasuhiroTaniguchi <yasuhirotaniguchi19950317@gmail.com>
2021-08-18 02:45:59 +0900

    feat: タイムライン構成まで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,12 +23,93 @@ const Home: React.VFC = () => {
               />
             </LogoArea>
             <MainArea>
-              <Item>ホーム</Item>
-              <Item>話題の検索</Item>
-              <Item>通知</Item>
-              <Item>メッセージ</Item>
-              <Item>ブックマーク</Item>
-              <Item>リスト</Item>
+              <Item>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M22.58 7.35L12.475 1.897c-.297-.16-.654-.16-.95 0L1.425 7.35c-.486.264-.667.87-.405 1.356.18.335.525.525.88.525.16 0 .324-.038.475-.12l.734-.396 1.59 11.25c.216 1.214 1.31 2.062 2.66 2.062h9.282c1.35 0 2.444-.848 2.662-2.088l1.588-11.225.737.398c.485.263 1.092.082 1.354-.404.263-.486.08-1.093-.404-1.355zM12 15.435c-1.795 0-3.25-1.455-3.25-3.25s1.455-3.25 3.25-3.25 3.25 1.455 3.25 3.25-1.455 3.25-3.25 3.25z"
+                    fill=" #d9d9d9"
+                  ></path>
+                </svg>
+                <ItemText>ホーム</ItemText>
+              </Item>
+              <Item>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M21 7.337h-3.93l.372-4.272c.036-.412-.27-.775-.682-.812-.417-.03-.776.27-.812.683l-.383 4.4h-6.32l.37-4.27c.037-.413-.27-.776-.68-.813-.42-.03-.777.27-.813.683l-.382 4.4H3.782c-.414 0-.75.337-.75.75s.336.75.75.75H7.61l-.55 6.327H3c-.414 0-.75.336-.75.75s.336.75.75.75h3.93l-.372 4.272c-.036.412.27.775.682.812l.066.003c.385 0 .712-.295.746-.686l.383-4.4h6.32l-.37 4.27c-.036.413.27.776.682.813l.066.003c.385 0 .712-.295.746-.686l.382-4.4h3.957c.413 0 .75-.337.75-.75s-.337-.75-.75-.75H16.39l.55-6.327H21c.414 0 .75-.336.75-.75s-.336-.75-.75-.75zm-6.115 7.826h-6.32l.55-6.326h6.32l-.55 6.326z"
+                    fill=" #d9d9d9"
+                  ></path>
+                </svg>
+                <ItemText>話題の検索</ItemText>
+              </Item>
+              <Item>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M21.697 16.468c-.02-.016-2.14-1.64-2.103-6.03.02-2.532-.812-4.782-2.347-6.335C15.872 2.71 14.01 1.94 12.005 1.93h-.013c-2.004.01-3.866.78-5.242 2.174-1.534 1.553-2.368 3.802-2.346 6.334.037 4.33-2.02 5.967-2.102 6.03-.26.193-.366.53-.265.838.102.308.39.515.712.515h4.92c.102 2.31 1.997 4.16 4.33 4.16s4.226-1.85 4.327-4.16h4.922c.322 0 .61-.206.71-.514.103-.307-.003-.645-.263-.838zM12 20.478c-1.505 0-2.73-1.177-2.828-2.658h5.656c-.1 1.48-1.323 2.66-2.828 2.66zM4.38 16.32c.74-1.132 1.548-3.028 1.524-5.896-.018-2.16.644-3.982 1.913-5.267C8.91 4.05 10.397 3.437 12 3.43c1.603.008 3.087.62 4.18 1.728 1.27 1.285 1.933 3.106 1.915 5.267-.024 2.868.785 4.765 1.525 5.896H4.38z"
+                    fill=" #d9d9d9"
+                  ></path>
+                </svg>
+                <ItemText>通知</ItemText>
+              </Item>
+              <Item>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M19.25 3.018H4.75C3.233 3.018 2 4.252 2 5.77v12.495c0 1.518 1.233 2.753 2.75 2.753h14.5c1.517 0 2.75-1.235 2.75-2.753V5.77c0-1.518-1.233-2.752-2.75-2.752zm-14.5 1.5h14.5c.69 0 1.25.56 1.25 1.25v.714l-8.05 5.367c-.273.18-.626.182-.9-.002L3.5 6.482v-.714c0-.69.56-1.25 1.25-1.25zm14.5 14.998H4.75c-.69 0-1.25-.56-1.25-1.25V8.24l7.24 4.83c.383.256.822.384 1.26.384.44 0 .877-.128 1.26-.383l7.24-4.83v10.022c0 .69-.56 1.25-1.25 1.25z"
+                    fill=" #d9d9d9"
+                  ></path>
+                </svg>
+                <ItemText>メッセージ</ItemText>
+              </Item>
+              <Item>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M12 11.816c1.355 0 2.872-.15 3.84-1.256.814-.93 1.078-2.368.806-4.392-.38-2.825-2.117-4.512-4.646-4.512S7.734 3.343 7.354 6.17c-.272 2.022-.008 3.46.806 4.39.968 1.107 2.485 1.256 3.84 1.256zM8.84 6.368c.162-1.2.787-3.212 3.16-3.212s2.998 2.013 3.16 3.212c.207 1.55.057 2.627-.45 3.205-.455.52-1.266.743-2.71.743s-2.255-.223-2.71-.743c-.507-.578-.657-1.656-.45-3.205zm11.44 12.868c-.877-3.526-4.282-5.99-8.28-5.99s-7.403 2.464-8.28 5.99c-.172.692-.028 1.4.395 1.94.408.52 1.04.82 1.733.82h12.304c.693 0 1.325-.3 1.733-.82.424-.54.567-1.247.394-1.94zm-1.576 1.016c-.126.16-.316.246-.552.246H5.848c-.235 0-.426-.085-.552-.246-.137-.174-.18-.412-.12-.654.71-2.855 3.517-4.85 6.824-4.85s6.114 1.994 6.824 4.85c.06.242.017.48-.12.654z"
+                    fill=" #d9d9d9"
+                  ></path>
+                </svg>
+                <ItemText>プロフィール</ItemText>
+              </Item>
+              <Item>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <circle cx="17" cy="12" r="1.5" fill=" #d9d9d9"></circle>
+                  <circle cx="12" cy="12" r="1.5" fill=" #d9d9d9"></circle>
+                  <circle cx="7" cy="12" r="1.5" fill=" #d9d9d9"></circle>
+                  <path
+                    d="M12 22.75C6.072 22.75 1.25 17.928 1.25 12S6.072 1.25 12 1.25 22.75 6.072 22.75 12 17.928 22.75 12 22.75zm0-20C6.9 2.75 2.75 6.9 2.75 12S6.9 21.25 12 21.25s9.25-4.15 9.25-9.25S17.1 2.75 12 2.75z"
+                    fill=" #d9d9d9"
+                  ></path>
+                </svg>
+                <ItemText>もっとみる</ItemText>
+              </Item>
               <Item>
                 <ButtonArea>ツイートする</ButtonArea>
               </Item>
@@ -59,12 +140,105 @@ const Home: React.VFC = () => {
                 <TweetTextUnderLine></TweetTextUnderLine>
               </TweetAreaUpper>
               <TweetAreaLower>
-                <IconArea>写像</IconArea>
+                <IconArea>
+                  <IconAreaSpan>
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M19.75 2H4.25C3.01 2 2 3.01 2 4.25v15.5C2 20.99 3.01 22 4.25 22h15.5c1.24 0 2.25-1.01 2.25-2.25V4.25C22 3.01 20.99 2 19.75 2zM4.25 3.5h15.5c.413 0 .75.337.75.75v9.676l-3.858-3.858c-.14-.14-.33-.22-.53-.22h-.003c-.2 0-.393.08-.532.224l-4.317 4.384-1.813-1.806c-.14-.14-.33-.22-.53-.22-.193-.03-.395.08-.535.227L3.5 17.642V4.25c0-.413.337-.75.75-.75zm-.744 16.28l5.418-5.534 6.282 6.254H4.25c-.402 0-.727-.322-.744-.72zm16.244.72h-2.42l-5.007-4.987 3.792-3.85 4.385 4.384v3.703c0 .413-.337.75-.75.75z"
+                        fill=" #1da1f2"
+                      ></path>
+                    </svg>
+                  </IconAreaSpan>
+                  <IconAreaSpan>
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M19 10.5V8.8h-4.4v6.4h1.7v-2h2v-1.7h-2v-1H19zm-7.3-1.7h1.7v6.4h-1.7V8.8zm-3.6 1.6c.4 0 .9.2 1.2.5l1.2-1C9.9 9.2 9 8.8 8.1 8.8c-1.8 0-3.2 1.4-3.2 3.2s1.4 3.2 3.2 3.2c1 0 1.8-.4 2.4-1.1v-2.5H7.7v1.2h1.2v.6c-.2.1-.5.2-.8.2-.9 0-1.6-.7-1.6-1.6 0-.8.7-1.6 1.6-1.6z"
+                        fill=" #1da1f2"
+                      ></path>
+                      <path
+                        d="M20.5 2.02h-17c-1.24 0-2.25 1.007-2.25 2.247v15.507c0 1.238 1.01 2.246 2.25 2.246h17c1.24 0 2.25-1.008 2.25-2.246V4.267c0-1.24-1.01-2.247-2.25-2.247zm.75 17.754c0 .41-.336.746-.75.746h-17c-.414 0-.75-.336-.75-.746V4.267c0-.412.336-.747.75-.747h17c.414 0 .75.335.75.747v15.507z"
+                        fill=" #1da1f2"
+                      ></path>
+                    </svg>
+                  </IconAreaSpan>
+                  <IconAreaSpan>
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M20.222 9.16h-1.334c.015-.09.028-.182.028-.277V6.57c0-.98-.797-1.777-1.778-1.777H3.5V3.358c0-.414-.336-.75-.75-.75s-.75.336-.75.75V20.83c0 .415.336.75.75.75s.75-.335.75-.75v-1.434h10.556c.98 0 1.778-.797 1.778-1.777v-2.313c0-.095-.014-.187-.028-.278h4.417c.98 0 1.778-.798 1.778-1.778v-2.31c0-.983-.797-1.78-1.778-1.78zM17.14 6.293c.152 0 .277.124.277.277v2.31c0 .154-.125.28-.278.28H3.5V6.29h13.64zm-2.807 9.014v2.312c0 .153-.125.277-.278.277H3.5v-2.868h10.556c.153 0 .277.126.277.28zM20.5 13.25c0 .153-.125.277-.278.277H3.5V10.66h16.722c.153 0 .278.124.278.277v2.313z"
+                        fill=" #1da1f2"
+                      ></path>
+                    </svg>
+                  </IconAreaSpan>
+                  <IconAreaSpan>
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M12 22.75C6.072 22.75 1.25 17.928 1.25 12S6.072 1.25 12 1.25 22.75 6.072 22.75 12 17.928 22.75 12 22.75zm0-20C6.9 2.75 2.75 6.9 2.75 12S6.9 21.25 12 21.25s9.25-4.15 9.25-9.25S17.1 2.75 12 2.75z"
+                        fill=" #1da1f2"
+                      ></path>
+                      <path
+                        d="M12 17.115c-1.892 0-3.633-.95-4.656-2.544-.224-.348-.123-.81.226-1.035.348-.226.812-.124 1.036.226.747 1.162 2.016 1.855 3.395 1.855s2.648-.693 3.396-1.854c.224-.35.688-.45 1.036-.225.35.224.45.688.226 1.036-1.025 1.594-2.766 2.545-4.658 2.545z"
+                        fill=" #1da1f2"
+                      ></path>
+                      <circle
+                        cx="14.738"
+                        cy="9.458"
+                        r="1.478"
+                        fill=" #1da1f2"
+                      ></circle>
+                      <circle
+                        cx="9.262"
+                        cy="9.458"
+                        r="1.478"
+                        fill=" #1da1f2"
+                      ></circle>
+                    </svg>
+                  </IconAreaSpan>
+                  <IconAreaSpan>
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M19.75 2H4.25C3.01 2 2 3.01 2 4.25v15.5C2 20.99 3.01 22 4.25 22h15.5c1.24 0 2.25-1.01 2.25-2.25V4.25C22 3.01 20.99 2 19.75 2zM4.25 3.5h15.5c.413 0 .75.337.75.75v9.676l-3.858-3.858c-.14-.14-.33-.22-.53-.22h-.003c-.2 0-.393.08-.532.224l-4.317 4.384-1.813-1.806c-.14-.14-.33-.22-.53-.22-.193-.03-.395.08-.535.227L3.5 17.642V4.25c0-.413.337-.75.75-.75zm-.744 16.28l5.418-5.534 6.282 6.254H4.25c-.402 0-.727-.322-.744-.72zm16.244.72h-2.42l-5.007-4.987 3.792-3.85 4.385 4.384v3.703c0 .413-.337.75-.75.75z"
+                        fill=" #1da1f2"
+                      ></path>
+                    </svg>
+                  </IconAreaSpan>
+                </IconArea>
                 <TweetButton>ツイートする</TweetButton>
               </TweetAreaLower>
             </TweetArea>
           </MyTweet>
-          <Timeline>タイムライン</Timeline>
+          <Timeline>
+            <TimeLineIcon>アイコン</TimeLineIcon>
+            <TimeLineArea>
+              <ProfileName>名前</ProfileName>
+              <SomeTweet>誰かのツイート</SomeTweet>
+              <ReplyIcon>リプライアイコン</ReplyIcon>
+            </TimeLineArea>
+          </Timeline>
         </Wrapper>
       </Container>
     </>
@@ -92,14 +266,23 @@ const UpperNav = styled.div``;
 const LowerNav = styled.div``;
 
 const Item = styled.div`
+  display: flex;
+  align-items: center;
   font-size: 2rem;
   color: #d9d9d9;
+  :hover {
+    background-color: rgba(29, 161, 242, 0.1);
+  }
+  cursor: pointer;
+`;
+const ItemText = styled.span`
+  margin-left: 2rem;
 `;
 const ButtonArea = styled.button`
   width: 14rem;
   height: 4.6rem;
   background: #1a91da;
-  border-radius: 10px;
+  border-radius: 30px;
   border: transparent;
   color: #ffffff;
   font-size: 1.5rem;
@@ -115,22 +298,20 @@ const MyTweet = styled.div`
   width: 60rem;
   height: 20rem;
   display: flex;
+  border-bottom: 1px solid #3b3a3a;
+  border-top: 1px solid #3b3a3a;
 `;
 const Wrapper = styled.div`
   width: 60rem;
   padding-left: 6rem;
 `;
-const Timeline = styled.div`
-  background: yellow;
-  width: 60rem;
-`;
+
 const LogoArea = styled.div`
-  height: 2em;
-  color: #d9d9d9;
+  height: 4em;
 `;
 const LogoIconItem = styled.img`
-  height: auto;
-  width: auto;
+  height: 4em;
+  width: 4em;
 `;
 
 const MainArea = styled.div`
@@ -142,21 +323,20 @@ const MainArea = styled.div`
 const ProfileArea = styled.div`
   width: 100%;
   height: 5rem;
-  border: 1px solid black;
+  border: 0.5px solid black;
   color: #d9d9d9;
 `;
 
 const ProfileIcon = styled.div`
-  background: blue;
   height: 100%;
   width: 12.2%;
 `;
 const ProfileIconItem = styled.img`
+  padding-top: 1rem;
   height: auto;
-  width: 100%;
+  width: 5rem;
 `;
 const TweetArea = styled.div`
-  background: red;
   height: 100%;
   width: 87.8%;
 `;
@@ -170,12 +350,17 @@ const TweetAreaLower = styled.div`
 `;
 const IconArea = styled.div`
   width: 70%;
+  display: flex;
+  align-items: center;
+`;
+const IconAreaSpan = styled.span`
+  margin-left: 2rem;
 `;
 const TweetButton = styled.button`
   width: 14rem;
   height: 4.6rem;
   background: #1a91da;
-  border-radius: 10px;
+  border-radius: 30px;
   border: transparent;
   color: #ffffff;
   font-size: 1.5rem;
@@ -203,6 +388,35 @@ const TweetTextInputSub = styled.button`
   cursor: pointer;
 `;
 const TweetTextUnderLine = styled.div`
+  border-bottom: 1px solid #3b3a3a;
   position: relative;
-  border-top: 1px solid #c2c2c2;
+`;
+
+const Timeline = styled.div`
+  color: #d9d9d9;
+  display: flex;
+  width: 60rem;
+  height: 15em;
+`;
+
+const TimeLineIcon = styled.div`
+  background-color: red;
+  height: 100%;
+  width: 12.2%;
+`;
+const TimeLineArea = styled.div`
+  height: 15rem;
+  width: 87.8%;
+`;
+const ProfileName = styled.div`
+  height: 20%;
+  background-color: green;
+`;
+const SomeTweet = styled.div`
+  height: 60%;
+  background-color: blue;
+`;
+const ReplyIcon = styled.div`
+  height: 20%;
+  background-color: yellow;
 `;
